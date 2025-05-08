b4bfb3f3c1540ff607e170aa490b9ddcbeb5e1e4
KhaydarovAkbar <khaydarov3255@gmail.com>
2025-05-08 11:53:18 +0500

    df

--- apps/academic_year/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AcademicYearConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.academic_year'
--- apps/base/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('static/countries/', views.CountryListAPIView.as_view(), name='country_list'),
-    path('static/regions/', views.RegionListAPIView.as_view(), name='region_list'),
-    path('static/districts/', views.DistrictsListAPIView.as_view(), name='district_list'),
-]
